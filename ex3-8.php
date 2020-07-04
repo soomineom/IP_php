5368881d9984be5425321893bc514c33ef4cc20d
Soomin Julia <34619357+tnals5718@users.noreply.github.com>
2020-07-04 15:53:07 +0900

    using _END

---
+++ ex3-8.php
@@ -0,0 +1,11 @@
+<?php
+  $author = "Brain W";
+
+  echo <<<_END
+  Debugging is twice as writting the code in the first place.
+  Therefore, if you write the code as clever as possible,
+  to debug it.
+
+  - $author.
+_END;
+ ?>
