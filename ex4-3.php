29adaa001882c54ce63f69d8caf1813165c94a16
Soomin Julia <34619357+soomineom@users.noreply.github.com>
2020-07-06 01:12:14 +0900

    여러가지 echo

---
+++ ex4-3.php
@@ -0,0 +1,13 @@
+<?php
+  $myname = "Bill";
+  $myage = 37;
+  $namaAge = $myname + $myage;
+
+  echo $namaAge . "<br>";
+  echo "a: " . 73      . "<br>";
+  echo "b: " . "Hello" . "<br>";
+  echo "c: " . FALSE   . "<br>";
+  echo "d: " . $myname . "<br>";
+  echo "e: " . $myage  . "<br>" ;
+
+ ?>
