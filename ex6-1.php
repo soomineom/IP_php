ae6bd970c05c537037548676d4a9bc8ae0e899b1
Soomin Julia <34619357+soomineom@users.noreply.github.com>
2020-07-09 10:39:25 +0900

    array

---
+++ ex6-1.php
@@ -0,0 +1,15 @@
+<?php
+  $paper[] = "Copier";
+  $paper[] = "Inkjet";
+  $paper[] = "Laser";
+  $paper[] = "Photo";
+
+  print_r($paper);
+
+  $paper1[0] = "Copier";
+  $paper1[2] = "Inkjet";
+  $paper1[1] = "Laser";
+  $paper1[3] = "Photo";
+
+  print_r($paper1)
+ ?>
