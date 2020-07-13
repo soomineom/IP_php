8b4eef9ee21f7c770cc5dd8b6b4d2a2cdbdee038
Soomin Julia <34619357+soomineom@users.noreply.github.com>
2020-07-14 02:57:29 +0900

    creating a table called cats

---
+++ ch10~/ex10-7.php
@@ -0,0 +1,17 @@
+
+<?php
+  require_once 'ex10-1.php'; //login.php
+  $conn = new mysqli($hn, $un, $pw, $db);
+  if ($conn->connect_error) die("Fatal Error");
+
+  $query = "CREATE TABLE cats (
+    id SMALLINT NOT NULL AUTO_INCREMENT,
+    family VARCHAR(32) NOT NULL,
+    name VARCHAR(32) NOT NULL,
+    age TINYINT NOT NULL,
+    PRIMARY KEY (id)
+  )";
+
+  $result = $conn->query($query);
+  if (!$result) die ("Database access failed");
+ ?>
