001744cb683ccedecc846faeb70d82207b836a9c
Soomin Julia <34619357+soomineom@users.noreply.github.com>
2020-07-14 14:52:04 +0900

    retrieving rows from the cats table

---
+++ ch10~/ex10-11.php
@@ -0,0 +1,24 @@
+
+<?php
+  require_once 'ex10-1.php'; //login.php
+  $conn = new mysqli($hn, $un, $pw, $db);
+  if ($conn->connect_error) die("Fatal Error");
+
+  $query  = "SELECT * FROM cats";
+  $result = $conn->query($query);
+  if (!$result) die ("Database access failed");
+
+  $rows = $result->num_rows;
+  echo "<table><tr> <th>Id</th> <th>Family</th><th>Name</th><th>Age</th></tr>";
+
+  for ($j = 0 ; $j < $rows ; ++$j)  {
+    $row = $result->fetch_array(MYSQLI_NUM);
+
+    echo "<tr>";
+    for ($k = 0 ; $k < 4 ; ++$k)
+      echo "<td>" . htmlspecialchars($row[$k]) . "</td>";
+    echo "</tr>";
+  }
+  
+  echo "</table>";
+ ?>
