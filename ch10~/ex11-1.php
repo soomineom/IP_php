505757aa6e201526592bcdb45a485829ca71cc2e
Soomin Julia <34619357+soomineom@users.noreply.github.com>
2020-07-15 02:24:42 +0900

    formtest.php

---
+++ ch10~/ex11-1.php
@@ -0,0 +1,18 @@
+
+<?php
+echo <<<_END
+  <html>
+    <head>
+      <title>Form test</title>
+    </head>
+    <body>
+    <form method="post" action="ex11-1.php">
+      What is your name?
+      <input type="text" name="name">
+      <input type="submit">
+    </form>
+    </body>
+  </html>
+
+_END;
+ ?>
